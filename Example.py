acdeb3e2bf3d6faab222263e07e51533b10db15b
John Dela Cruz <kenneth1521412@gmail.com>
2020-07-16 23:57:11 +0100

    Challenge complete

---
+++ Example.py
@@ -0,0 +1,6 @@
+july_names = ["Christopher", "Samuel", "Steven", "Christopher", "John", "Bradley", "Wasim", "Domenico", "Jacob", "Diarmuid", "Joshua", "Tobias", "Amanda", "Arsalan", "Clifford", "Mohamed", "Sithembiso", "Edmund", "Javas", "Jason", "Ryan"]
+july_names.append("Luke")
+print(july_names)
+print(july_names[4])
+print("Christopher:", july_names.count("Christopher"))
+print("Chris:", july_names.count("Chris"))
